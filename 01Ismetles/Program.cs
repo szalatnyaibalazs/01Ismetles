fbf392bee6d2c69ef842804711d8ed3c1a4ddfa8
szalatnyaibalazs <szalatnyaibalazs@gamil.com>
2020-09-09 14:18:47 +0200

    Add project files.

---
+++ 01Ismetles/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Ismetles
+{
+    class Program
+    {
+
+        static int EmberNyer(int gep, int ember)
+        {
+            if (ember == gep) // Döntetlen
+            {
+                return 0;
+            }
+            else if (
+                        (ember == 0 && gep == 1)
+                        ||
+                        (ember == 1 && gep == 2)
+                        ||
+                        (ember == 2 && gep == 0)
+                    ) // Gép nyer
+            {
+                return 1;
+            }
+            else // Játékos nyer
+            {
+                return 2;
+            }
+        }
+
+        static void Main()
+        {
+            Random vel = new Random();
+            string[] lehetoseg = new string[] { "Kő", "Papír", "Olló" };
+
+            int gepValasz = vel.Next(0, 3 );
+
+            int jatekosValasz;
+
+            Console.WriteLine("Kő (0), Papír (1), Olló (2)");
+            Console.Write("Válassz: ");
+            jatekosValasz = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Gép: {0} --- Játékos: {1}",
+                                lehetoseg[gepValasz], lehetoseg[jatekosValasz]
+                    );
+            
+            switch (EmberNyer(gepValasz, jatekosValasz))
+            {
+                case 0:
+                    Console.WriteLine("Döntetlen!");
+                    break;
+                case 1:
+                    Console.WriteLine("Skynet nyert!");
+                    break;
+                case 2:
+                    Console.WriteLine("Játékos nyert!");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Játékprogram - (Kő, Papír, Olló)
+
+## Játékmenet 1.0
+- [x] A számítógép választ egy lehetőséget
+- [X] A felhasználó megad egy lehetőséget
+- Nyerés:
+	- Kő legyőzi az Ollót
+	- Papír legyőzi a Követ
+	- Olló legyőzi a Papírt
+
+	[] A nyerés megállapítását kiszerverzzük metódusba
+	- EmberNyer()
+    - visszaadott érték -> 0, 1, 2
+    - 0: Döntetlen
+    - 1: Gép nyer
+    - 2: Ember nyer
+- [x] Kiírjuk az eredményt
